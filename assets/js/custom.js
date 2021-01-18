785752dffd280af0bd92deab4a9dd45c894535a4
Clint Mossman <clintmossman808@gmail.com>
2021-01-18 12:44:35 -0700

    add footer links and change page titles

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+// class User {
+//   constructor(name, email, message) {
+//   this.name = name;
+//   this.email = email;
+//   this.message = message;
+//   this.email = email;
+//   }
+//  }
+
+
+
