addef728f6a8d4e637991a573518775c6610217e
Clint Mossman <clintmossman808@gmail.com>
2021-01-28 11:14:17 -0700

    Add API Key and test email

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ require 'vendor/autoload.php';
 // If you are not using Composer (recommended)
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
-$from = new SendGrid\Email(null, "test@example.com");
+$from = new SendGrid\Email(null, "clint.mossman@gmail.com");
 $subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "test@example.com");
+$to = new SendGrid\Email(null, "clint.mossman@gmail.com");
 $content = new SendGrid\Content("text/plain", "Hello, Email!");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$apiKey = getenv('PNABHrDDTqGC5iyDLsw9Ag');
+$apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
