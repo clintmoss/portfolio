bd44e6024ceabbd4f26d1ff12975c9566276c7e6
Clint Mossman <clintmossman808@gmail.com>
2021-01-29 11:31:39 -0700

    adding email field to contact form

--- contact-form.php
+++ contact-form.php
@@ -7,21 +7,25 @@ require 'vendor/autoload.php';
 
 $name = $_POST['name']; 
 $message = $_POST['message'];
+$email = $_POST['email'];
+$thankyou = 'Thank you! Redirecting you back to my portfolio...';
 
 $from = new SendGrid\Email($name, "clint.mossman@gmail.com");
 $subject = "Portfolio Message";
 $to = new SendGrid\Email(null, "clint.mossman@gmail.com");
-$content = new SendGrid\Content("text/plain", $message);
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$content = new SendGrid\Content("text/plain", $message + $email);
+$mail = new SendGrid\Mail($from, $subject, $to, $content,);
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
+sleep(3);
+echo $thankyou;
 header('Location: https://clintmossman-portfolio.herokuapp.com/');
 exit();
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+// echo $response->statusCode();
+// echo $response->headers();
+// echo $response->body();
 
 ?>
